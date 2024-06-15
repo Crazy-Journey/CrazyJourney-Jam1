c7074de118299ecead913fab2f285601cb5ba678
Lparres2000 <Lparres2000@gmail.com>
2024-06-15 10:58:12 +0200

    PlayerDataManager Base

--- Assets/Scripts/PlayerDataManager.cs
+++ Assets/Scripts/PlayerDataManager.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class PlayerDataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PlayerDataManager THIS;
+
+    public struct PlayerData
     {
-        
+        private float power;
+        private int coins;
+
+        public void SetPower(float p)
+        {
+            power = p;
+        }
+
+        public void ChangePower(float p)
+        {
+            power += p;
+        }
+
+        public void SetCoins(int c)
+        {
+            coins = c;
+        }
+
+        public void ChangeCoins(int c)
+        {
+            coins += c;
+        }
+
+        public float GetPower()
+        {
+            return power;
+        }
+
+        public int GetCoins()
+        {
+            return coins;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] PlayerData playerData1;
+    [SerializeField] PlayerData playerData2;
+
+    private void Awake()
     {
-        
+        // Uso de singleton
+        if (THIS == null)
+        {
+            THIS = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
     }
+
+    public PlayerData Player1()
+    {
+        return playerData1;
+    }
+
+    public PlayerData Player2()
+    {
+        return playerData2;
+    }
+
 }
