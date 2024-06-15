e3db05a0e7117827e3ca5a25536f191856bc6958
DynamicZero <dynamic.cast99@gmail.com>
2024-06-15 20:03:35 +0200

    Stats UI 100% operative

--- Assets/Scripts/Shooting/LifeComponent.cs
+++ Assets/Scripts/Shooting/LifeComponent.cs
@@ -130,7 +130,7 @@ public class LifeComponent : MonoBehaviour
             _player.ChangeCoins(+manager.coinDrop);
 
             // Actualizamos UI del jugador
-            if (bulletOwnerId == 0)
+            /* if (bulletOwnerId == 0)
             {
                 GameObject.Find("CoinsText1").GetComponent<TMP_Text>().text = _player.GetCoins().ToString();
                 GameObject.Find("PowerText1").GetComponent<TMP_Text>().text = _player.GetPower().ToString();
@@ -140,7 +140,7 @@ public class LifeComponent : MonoBehaviour
             {
                 GameObject.Find("CoinsText2").GetComponent<TMP_Text>().text = _player.GetCoins().ToString();
                 GameObject.Find("PowerText2").GetComponent<TMP_Text>().text = _player.GetPower().ToString();
-            }
+            } */
 
             PlayerDataManager.THIS.SetPlayer(bulletOwnerId, _player);
             Destroy(gameObject);
