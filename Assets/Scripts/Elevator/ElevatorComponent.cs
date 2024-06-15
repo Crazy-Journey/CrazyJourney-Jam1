48a787bb7493b6f13ed3b52efcb615c8ceb4df7a
SamuelGarciaBazan <samuga05@ucm.es>
2024-06-15 11:05:48 +0200

    detect player

--- Assets/Scripts/Elevator/ElevatorComponent.cs
+++ Assets/Scripts/Elevator/ElevatorComponent.cs
@@ -12,19 +12,47 @@ public class ElevatorComponent : MonoBehaviour
     private int Nfloors;
 
     [SerializeField]
-    List<int> floorCosts = new List<int>();
+    private List<int> floorCosts = new List<int>();
 
+    [SerializeField]
+    private Transform RaycastOrigin;
 
+    [SerializeField]
+    private Vector2 RaycastDir;
+
+    [SerializeField]
+    private float RaycastDistance;
+
+    [SerializeField]
+    private bool PlayerDetected;
+
+    [SerializeField]
+    private LayerMask playerMask; 
 
     public void SubirPiso(int cantidad = 1)
     {
         currentFloor -= cantidad;
 
+        transition();
     }
 
     public void BajarPiso()
     {
         currentFloor += 1;
+        transition();
+    }
+
+    private void transition()
+    {
+
+    }
+
+    private void detectPlayer()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(RaycastOrigin.position, RaycastDir, RaycastDistance, playerMask);
+
+        PlayerDetected = hit.rigidbody != null;       
     }
 
     private int FormulaEscalado(int n)
@@ -44,6 +72,6 @@ public class ElevatorComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        detectPlayer();
     }
 }
