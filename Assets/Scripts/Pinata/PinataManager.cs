e7c9f59811878a181260c61e126fcb25861d275e
samuel <samuga05@ucm.es>
2024-07-02 17:02:59 +0200

    puntos cuando matas + arreglo spawners

--- Assets/Scripts/Pinata/PinataManager.cs
+++ Assets/Scripts/Pinata/PinataManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class PinataManager : MonoBehaviour
 {
     public float powerDrop;
     public int coinDrop;
     [SerializeField] GameObject particleEffect;
+
+    [SerializeField] GameObject scoreVisual;
+    [SerializeField] Sprite spriteCoin;
+    [SerializeField] Sprite spritePower;
 
     private EnemySpawner spawner;
 
@@ -19,7 +26,25 @@ public class PinataManager : MonoBehaviour
     private void OnDestroy()
     {
         spawner.PinataDied();
-        GameObject effect = Instantiate(particleEffect, transform.position, Quaternion.identity) as GameObject;
+        GameObject effect = Instantiate(particleEffect, transform.position, Quaternion.identity);
         Destroy(effect, 2f);
+
+
+        GameObject _scoreVisual = Instantiate(scoreVisual, transform.position, Quaternion.identity);
+
+        if(coinDrop > 0)
+        {
+
+            _scoreVisual.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>().sprite = spriteCoin;
+            _scoreVisual.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<TMP_Text>().text = "+" + coinDrop.ToString();
+        }
+        else
+        {
+            _scoreVisual.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>().sprite = spritePower;
+            _scoreVisual.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<TMP_Text>().text = "+" + powerDrop.ToString();
+        }
+
+        Destroy(_scoreVisual, 1f);
+
     }
 }
